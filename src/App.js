783e54f6f98881df53af3420132793c9c06716ae
James Enoch <jamesenoch@Jamess-MBP.attlocal.net>
2022-05-29 19:01:20 -0700

    add button, link for create listing page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Category from './pages/Category';
 import Profile from './pages/Profile';
+import CreateListing from './pages/CreateListing';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Navbar from './components/Navbar';
@@ -22,6 +23,7 @@ function App() {
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
---
+++ src/pages/CreateListing.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CreateListing(props) {
+  return <div>New Listing</div>;
+}
+
+export default CreateListing;
